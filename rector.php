67d3af8876ed4da1ebd04f6f0e51b975010185d9
Crypta Electrica <crypta@crypta.tech>
2024-11-15 22:11:35 +1030

    style: add rector

---
+++ rector.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+use Rector\Set\ValueObject\LevelSetList;
+use RectorLaravel\Set\LaravelLevelSetList;
+use RectorLaravel\Set\LaravelSetList;
+
+return RectorConfig::configure()
+    ->withPaths([__DIR__ . '/src'])
+    ->withSets([
+        LaravelLevelSetList::UP_TO_LARAVEL_100,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_COLLECTION,
+        LaravelSetList::LARAVEL_CONTAINER_STRING_TO_FULLY_QUALIFIED_NAME,
+        LaravelSetList::LARAVEL_IF_HELPERS,
+        LaravelSetList::LARAVEL_LEGACY_FACTORIES_TO_CLASSES,
+        SetList::PHP_83,
+        LevelSetList::UP_TO_PHP_83,
+    ])
+    ->withImportNames(removeUnusedImports: true);
